006ffc3aed87cd37d47aae016e14d8a98ddc3ec9
AshwinDeshpande96 <ashwindeshpande96@gmail.com>
2022-01-24 16:24:10 -0600

    Cassandra reader Scala

---
+++ Cassandra_Reader/src/main/scala/WeatherReader.scala
@@ -0,0 +1,26 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */
+import com.datastax.oss.driver.api.core.CqlSession
+
+import scala.jdk.CollectionConverters._
+
+object SampleConnectionWithSigv4 {
+
+  // This code uses the configuration present in ./resources/application.conf
+  // and the AWS default credential chain.
+  // see https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html.
+  def main(args: Array[String]): Unit = {
+    val resultSet = session.execute("select * from weather_keyspace.weather");
+    val rows = resultSet.all().asScala;
+    rows.foreach({ println } );
+
+    println("Weather table has following rows:\nuuid,device,temp,humd,pres\n");
+    for (row <- rows) println("%s,%s,%d,%d,%d" format (row.getUUID("uuid").as[String], row.getUUID("device"), row.getUUID("temp"), row.getUUID("humd"), row.getUUID("pres")));
+
+    System.exit(0);
+  }
+
+  private val session = CqlSession.builder.build()
+}
